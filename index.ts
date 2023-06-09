7af30275e33fc67c891b9776fdec102067904d8e
Patrick Wozniak <email@patwoz.de>
2023-06-09 16:57:42 +0200

    adding example code

--- index.ts
+++ index.ts
@@ -1,1 +1,32 @@
-console.log("Hello via Bun!");
+import fs from 'fs'
+import initYoga from 'yoga-wasm-web'
+
+const yoga = await initYoga(
+  fs.readFileSync('./node_modules/yoga-wasm-web/dist/yoga.wasm')
+)
+
+const Node = yoga.Node
+
+const root = Node.create();
+root.setWidth(500);
+root.setHeight(300);
+root.setJustifyContent(yoga.JUSTIFY_CENTER);
+
+const node1 = Node.create();
+node1.setWidth(100);
+node1.setHeight(100);
+
+const node2 = Node.create();
+node2.setWidth(100);
+node2.setHeight(100);
+
+root.insertChild(node1, 0);
+root.insertChild(node2, 1);
+
+root.calculateLayout(500, 300, yoga.DIRECTION_LTR);
+console.log(root.getComputedLayout());
+// {left: 0, top: 0, width: 500, height: 300}
+console.log(node1.getComputedLayout());
+// {left: 150, top: 0, width: 100, height: 100}
+console.log(node2.getComputedLayout());
+  // {left: 250, top: 0, width: 100, height: 100}
